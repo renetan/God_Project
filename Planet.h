0b2cff7c1d4b3fdfcd28b752cdc8a978c739982a
renetan <nikolcho.r@gmail.com>
2015-06-27 00:59:46 +0300

    Update Planet.h

--- Planet.h
+++ Planet.h
@@ -12,14 +12,14 @@ private:
  string name;
 	
 public:
- Planet(string name);
- string getName()const { return this->name; }
- void setName(string name) { this->name=name; }
- int getPopulation()const { return population.size(); }
- void destroy();
- void add(Entity* e);
+ Planet(string name);    
+ string getName()const{ return this->name; }
+ void setName(string name) { this->name = name; }
+ int getPopulation()const{ return population.size(); }
+ void erasePopulation();
+ void addPopulation(Entity* e, int number);
  ~Planet();
-  
+
 };
 
 #endif
