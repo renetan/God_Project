e324ff1fe37ed0eabdb61331b842318b2f43ffd4
renetan <nikolcho.r@gmail.com>
2015-06-27 00:20:54 +0300

    Update Human.cpp

--- Human.cpp
+++ Human.cpp
@@ -1,16 +1,17 @@
 #include "Human.h"
-#include<iosteam>
+#include <iosteam>
 
 using namespace std;
 
 Human::Human() {}
 
-Human::Human(string name, double energy, double power, double size, double weight, Point2D point, State state) : Animal(name, energy, power, size, weight, point, state);
+Human::Human(string name, double energy, double power, double size, double weight, Point2D* point, State state) : Animal(name, energy, power, size, weight, point, state);
 {                
 }                   
 
-void Human::Analyze() {
-    this->setState(Analyzing);
-    RandomG mind;
-    this->power=mind.RandomNumber();
- }
+void Human::Analyze() 
+{
+ this->setState(Analyzing);
+ RandomG mind;
+ this->power=mind.RandomNumber();
+}
