21050e55d99e364245850e0413d64d193ba3e6b4
renetan <nikolcho.r@gmail.com>
2015-06-26 23:43:41 +0300

    Update Entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -12,7 +12,7 @@ Entity::Entity()
   this->power = 0;
   this->size = 0;
   this->weight = 0;
-  this->point = new Point2D();
+  point = new Point2D();
   this->state = State.Unknown;
 }
 
@@ -23,7 +23,7 @@ Entity::Entity(string name, double energy, double power, double size, double wei
   this->power = power;
   this->size = size;
   this->weight = weight;
-  this->point = new Point2D();
+  point = new Point2D();
   this->state = state.Unknown;               
 }                   
 
