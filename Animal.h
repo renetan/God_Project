918f9dc9b2cc616f36bbfca1d1bb609d2f5d6ce4
renetan <nikolcho.r@gmail.com>
2015-06-26 23:56:35 +0300

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -6,12 +6,12 @@
 class Animal : public Entity
 {
 public:
-  Animal();
-  Animal(string name,double energy,double power,double size,double weight, Point2D point,State state); 
+ Animal();
+ Animal(string name, double energy, double power, double size, double weight, Point2D* point, State state); 
 
-	void Eat();
-	void Sleep();
-	void SearchingForFood();
+ void Eat();
+ void Sleep();
+ void SearchingForFood();
 };
 
 #endif
