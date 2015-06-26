a2ad89329ca6565479c0160b5974c665706cb5ae
renetan <nikolcho.r@gmail.com>
2015-06-27 01:07:40 +0300

    Update Planet.cpp

--- Planet.cpp
+++ Planet.cpp
@@ -5,43 +5,48 @@ using namespace std;
 
 Planet::Planet(string name) 
 {
-	this->name = name;
+ this->name = name;
 }
 
 void Planet::erasePopulation()
 {
-	population.erase(population.begin(), population.begin() + population.size()); //unishtojava cqloto naselenie na planetata
+ population.erase(population.begin(), population.begin() + population.size()); //unishtojava cqloto naselenie na planetata
 }
 
 void Planet::addPopulation(EntityType* e, int number)
 {
-	for (int i = 0; i < number; i++)
-	{
-		switch (e)
-		{
-		 case entity:
-			population.push_back(new Entity()); break;   //push_back adds element to the end of the vector
-		 
-		 case animal:
-			population.push_back(new Animal()); break;
-		 
-		 case human:
-			population.push_back(new Human()); break;
-		 
-		 case god:
-			population.push_back(new God()); break;
-		 
-		 case unknown:
-		 population.push_back(new God()); break;
-		}
-	}
+ for (int i = 0; i < number; i++)
+ {
+  switch (e)
+  {
+   case entity:
+   population.push_back(new Entity()); 
+   break;   //push_back adds element to the end of the vector
+   
+   case animal:
+   population.push_back(new Animal()); 
+   break;
+   
+   case human:
+   population.push_back(new Human()); 
+   break;
+   
+   case god:
+   population.push_back(new God()); 
+   break;
+   
+   case unknown:
+   population.push_back(new God()); 
+   break;
+  }
+ }
 }
 
 Planet::~Planet()
 {
  while (!population.empty())
  {
-		delete population.back();   //"delete" erases the memory allocated for the object of the vector
-		population.pop_back();
-	}
+  delete population.back();   //"delete" erases the memory allocated for the object of the vector
+  population.pop_back();
+ }
 }
