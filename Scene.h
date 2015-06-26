13ed5d09237167f86c805dea8478b4675f92006b
renetan <nikolcho.r@gmail.com>
2015-06-27 00:36:12 +0300

    Update Scene.h

--- Scene.h
+++ Scene.h
@@ -6,13 +6,12 @@
 class Scene
 {
 private:
-	vector<Planet*> planets;
-
+ vector<Planet*> planets;
 public:
-	Scene();	
-	Scene(Planet* p);
-	void createEntity(EntityType creature, Planet p);
-	~Scene();
+ Scene();	
+ Scene(Planet* p);
+ void createEntity(EntityType creature, Planet p);
+ ~Scene();
 };
 
 #endif
