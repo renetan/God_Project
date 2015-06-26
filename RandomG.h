5fc43ab4e0c297cceb942451ab8e8e117f7541e7
renetan <nikolcho.r@gmail.com>
2015-06-27 00:31:47 +0300

    Update RandomG.h

--- RandomG.h
+++ RandomG.h
@@ -1,13 +1,15 @@
 #ifndef RANDOMG_H
 #define RANDOM_H
+
 #include "Entity.h"
 #include "Point2D.h"
 
 class RandomG 
 {
 public:
-  Point2D getRandomCordinates();
-  void getRandomDamage(Entity& smth);
-  double RandomNumber();
+ Point2D getRandomCordinates();
+ void getRandomDamage(Entity& smth);
+ double RandomNumber();
 };
+
 #endif
